7b8446360075cb8f1ece3b10cea66cc50a602d4a
Garrett Ransom <g2ransom@gmail.com>
2018-06-08 23:18:12 -0400

    added query functions and incorporated into main script

--- nytimes_scraper/archives_db.py
+++ nytimes_scraper/archives_db.py
@@ -4,11 +4,14 @@ import requests
 import multiprocessing as mp
 import os
 from pprint import pprint
+import pysolr
+import itertools
 
 
 key = os.environ['NYT_API']
 template = "http://api.nytimes.com/svc/archive/v1/{year}/{month}.json?api-key={api_key}"
-
+nyt_df = pd.read_csv('nytimes_corps.csv')
+corps = nyt_df.comnam.tolist()
 
 def archives_request(template, year, month, key=key):
 	url = template.format(year=year, month=month, api_key=key)
@@ -30,10 +33,6 @@ def parse_json(json_item):
 	except KeyError:
 			dic['abstract'] = 'None'
 	dic['headline'] = json_item['headline']['main'].encode('utf-8')
-	# if json_item['keywords'] is not None:
-	# 	dic['keywords'] = [json_item['keywords'][i]['value'] for i in range(len(json_item['keywords']))]
-	# else:
-	# 	dic['keywords'] = 'None'
 	dic['date'] = json_item['pub_date']
 	if json_item['snippet'] is not None:
 		dic['snippet'] = json_item['snippet'].encode('utf-8')
@@ -42,11 +41,6 @@ def parse_json(json_item):
 	dic['source'] = json_item['source']
 	dic['url'] = json_item['web_url']
 	return dic
-
-
-'''Filter each dataframe by company name and divestment indicators'''
-def filter_articles(df):
-	pass
 
 
 '''Drop new data into dataframe by chunks - month by month'''
@@ -69,14 +63,38 @@ def dataframe_tosql(dbname, conn, df):
 	df.to_sql(name=dbname, con=conn, if_exists="append", index=False)
 
 
+def query_ids(json_rows, company_name):
+	solr.delete(q='*:*')
+	solr.add(json_rows)
+	q = '(snippet: {company}) OR (headline: {company})'
+	fq = {'fq': """(snippet:divest) 
+				OR (snippet: divestment) 
+				OR (snippet:divested) 
+				OR (snippet:divestiture)
+				OR (snippet:blacklist)
+				OR (snippet:censor)
+				OR (snippet: firesale)
+				OR (snippet: fire sale)
+				OR (snippet: dump)"""}
+	results = solr.search(q.format(company=company_name), rows=100000, **fq)
+	return [result['id'] for result in results]
+
+
+def filter_rows(rows, ids):
+	df = pd.DataFrame(rows)
+	return df.loc[df['id'].isin(ids)]
+
+
 if __name__ == '__main__':
+	solr = pysolr.Solr('http://localhost:8983/solr/default/')
 	for year in range(1962, 2019):
-		for month in range(1, 13): 
+		for month in range(1, 13):
+			url_df = pd.DataFrame()
 			json = archives_request(template, year, month)
-			pool = mp.Pool(processes=4)
+			pool = mp.Pool(processes=10)
 			rows = [pool.apply(parse_json, args=(json_item,)) for json_item in json['response']['docs']]
-			df = pd.DataFrame(rows)
-			update_database("nytimes.db", "nytimes", dataframe_tosql, df)
-	print("SQL Dump Complete!")
-
-
+			ids = [pool.apply(query_ids, args=(rows, corp)) for corp in corps]
+			ids = list(itertools.chain(*ids))
+			temp_df = filter_rows(rows, ids)
+			url_df = url_df.append(temp_df, ignore_index=True)
+	url_df.to_csv('nyt_url_data.csv', index=False)
