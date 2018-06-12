50c2a86584f79eca6b9c669fa37b10e9ccd573d1
Garrett Ransom <g2ransom@gmail.com>
2018-06-11 22:34:14 -0400

    added postgres inserts and deleted solr searches

--- nytimes_scraper/archives_db.py
+++ nytimes_scraper/archives_db.py
@@ -1,100 +1,81 @@
 import pandas as pd
-import sqlite3
 import requests
+import time
 import multiprocessing as mp
 import os
 from pprint import pprint
 import pysolr
 import itertools
-
+import logging
+import json
+import psycopg2
+import sqlalchemy
 
 key = os.environ['NYT_API']
 template = "http://api.nytimes.com/svc/archive/v1/{year}/{month}.json?api-key={api_key}"
-nyt_df = pd.read_csv('nytimes_corps.csv')
-corps = nyt_df.comnam.tolist()
+
 
 def archives_request(template, year, month, key=key):
 	url = template.format(year=year, month=month, api_key=key)
 	try:
-		response = requests.get(url, timeout=5)
+		response = requests.get(url, timeout=10)
+		time.sleep(1)
 		return response.json()
-	except requests.exceptions.RequestException as e:
+	except (requests.exceptions.RequestException, ValueError) as e:
 		print(e, url)
 
 
 def parse_json(json_item):
 	dic = {}
 	dic['id'] = json_item['_id']
-	try:
-		if json_item['abstract'] is not None:
-			dic['abstract'] = json_item['abstract'].encode('utf-8')
-		else:
-			dic['abstract'] = 'None'
-	except KeyError:
-			dic['abstract'] = 'None'
 	dic['headline'] = json_item['headline']['main'].encode('utf-8')
 	dic['date'] = json_item['pub_date']
-	if json_item['snippet'] is not None:
-		dic['snippet'] = json_item['snippet'].encode('utf-8')
-	else:
+	try:
+		if json_item['snippet'] is not None:
+			dic['snippet'] = json_item['snippet'].encode('utf-8')
+		else:
+			dic['snippet'] = 'None'
+	except KeyError, TypeError:
 		dic['snippet'] = 'None'
-	dic['source'] = json_item['source']
 	dic['url'] = json_item['web_url']
 	return dic
 
 
-'''Drop new data into dataframe by chunks - month by month'''
-def update_database(dbfile, dbname, dataframe_sql_function, df):
+def psql_connect(conn_str):
 	try:
-		conn = sqlite3.connect(dbfile)
-		conn.text_factory = str
-		cur = conn.cursor()
+		conn = psycopg2.connect(conn_str)
 		return conn
-	except Error:
+	except psycopg2.Error as e:
 		conn.close()
-		print "Update Failure"
-	finally:
-		dataframe_sql_function(dbname, conn, df)
-		print "Update Success!"
-		conn.close()
+		print("Connection Error: %s" % e)
 
 
-def dataframe_tosql(dbname, conn, df):
+def df_topsql(dbname, conn, df):
 	df.to_sql(name=dbname, con=conn, if_exists="append", index=False)
 
 
-def query_ids(json_rows, company_name):
-	solr.delete(q='*:*')
-	solr.add(json_rows)
-	q = '(snippet: {company}) OR (headline: {company})'
-	fq = {'fq': """(snippet:divest) 
-				OR (snippet: divestment) 
-				OR (snippet:divested) 
-				OR (snippet:divestiture)
-				OR (snippet:blacklist)
-				OR (snippet:censor)
-				OR (snippet: firesale)
-				OR (snippet: fire sale)
-				OR (snippet: dump)"""}
-	results = solr.search(q.format(company=company_name), rows=100000, **fq)
-	return [result['id'] for result in results]
-
-
-def filter_rows(rows, ids):
-	df = pd.DataFrame(rows)
-	return df.loc[df['id'].isin(ids)]
-
-
 if __name__ == '__main__':
-	solr = pysolr.Solr('http://localhost:8983/solr/default/')
+	with open('config.json', 'r') as f:
+		conf = json.load(f)
+	conn_str = "host={} dbname={} user={} password={}".format(conf['host'], conf['database'], conf['user'], conf['password'])		
+	conn = psql_connect(conn_str)
+	cur = conn.cursor()
+	pool = mp.Pool(processes=10)
 	for year in range(1962, 2019):
-		for month in range(1, 13):
-			url_df = pd.DataFrame()
+		for month in range(1, 12):
 			json = archives_request(template, year, month)
-			pool = mp.Pool(processes=10)
-			rows = [pool.apply(parse_json, args=(json_item,)) for json_item in json['response']['docs']]
-			ids = [pool.apply(query_ids, args=(rows, corp)) for corp in corps]
-			ids = list(itertools.chain(*ids))
-			temp_df = filter_rows(rows, ids)
-			url_df = url_df.append(temp_df, ignore_index=True)
-	url_df.to_csv('nyt_url_data.csv', index=False)
+			try:
+				rows = [pool.apply(parse_json, args=(json_item,)) for json_item in json['response']['docs']]
+				values = [tuple(v for k,v in row.items()) for row in rows]
+				print("Starting postgresql writing process.")
+				args_str = ','.join(cur.mogrify("(%s, %s, %s, %s, %s)", x) for x in values)
+				try:
+					cur.execute("INSERT INTO nytimes (headline, date, snippet, url, id) VALUES " + args_str)
+					print("Success for year %i month %i." % (year, month))
+				except psycopg2.Error as e:
+					print("This error occurred: %s" % e)
+			except (TypeError, KeyError) as e:
+				print("Type Error occurred for year %i month %i: %s" % (year, month, e))
+			
+	print("Completed database dump of NY Times archives data.")
+	conn.close()
